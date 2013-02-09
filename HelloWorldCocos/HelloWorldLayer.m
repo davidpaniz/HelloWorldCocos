ef8beb7ff17469c742aa20b0be9c9fd6db79a4a0
David Paniz <davidpaniz@gmail.com>
2013-02-09 16:01:50 -0200

    first commit

---
+++ HelloWorldCocos/HelloWorldLayer.m
@@ -0,0 +1,109 @@
+//
+//  HelloWorldLayer.m
+//  HelloWorldCocos
+//
+//  Created by David Paniz on 2/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+// Import the interfaces
+#import "HelloWorldLayer.h"
+
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
+#import "CCTouchDispatcher.h"
+
+CCSprite *seeker1;
+CCSprite *cocosGuy;
+
+#pragma mark - HelloWorldLayer
+
+// HelloWorldLayer implementation
+@implementation HelloWorldLayer
+
+// Helper class method that creates a Scene with the HelloWorldLayer as the only child.
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	HelloWorldLayer *layer = [HelloWorldLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+	
+	// return the scene
+	return scene;
+}
+
+// on "init" you need to initialize your instance
+-(id) init
+{
+    // always call "super" init
+	// Apple recommends to re-assign "self" with the "super's" return value
+	if( (self=[super init]) ) {
+        self.isTouchEnabled = YES;
+        seeker1 = [CCSprite spriteWithFile:@"seeker.png"];
+        seeker1.position = ccp(50, 100);
+        [self addChild:seeker1];
+        
+        cocosGuy = [CCSprite spriteWithFile:@"Icon.png"];
+        cocosGuy.position = ccp(200, 300);
+        [self addChild:cocosGuy];
+        
+        // schedule a repeating callback on every frame
+        [self schedule:@selector(nextFrame:)];
+	}
+	return self;
+}
+
+- (void) nextFrame:(ccTime)dt {
+    seeker1.position = ccp( seeker1.position.x + 100*dt, seeker1.position.y );
+    if (seeker1.position.x > 480+32) {
+        seeker1.position = ccp( -32, seeker1.position.y );
+    }
+}
+
+// on "dealloc" you need to release all your retained objects
+- (void) dealloc
+{
+	// in case you have something to dealloc, do it in this method
+	// in this particular example nothing needs to be released.
+	// cocos2d will automatically release all the children (Label)
+	
+	// don't forget to call "super dealloc"
+	[super dealloc];
+}
+
+-(void) registerWithTouchDispatcher
+{
+	[[[CCDirector sharedDirector] touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
+}
+
+- (BOOL)ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    return YES;
+}
+
+- (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event {
+	CGPoint location = [self convertTouchToNodeSpace: touch];
+    
+	[cocosGuy stopAllActions];
+	[cocosGuy runAction: [CCMoveTo actionWithDuration:1 position:location]];
+}
+
+#pragma mark GameKit delegate
+
+-(void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
+
+-(void) leaderboardViewControllerDidFinish:(GKLeaderboardViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
+@end
